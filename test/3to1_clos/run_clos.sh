b0db61890a06acf113a494a17127b4500b0ca6c8
haoxiang01 <276063373@qq.com>
2025-12-27 01:39:49 +0000

    update

--- test/3to1_clos/run_clos.sh
+++ test/3to1_clos/run_clos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-../../src/clos/datacenter/htsim_ndp_fatTree_3to1_k12 -q 46 -cwnd 30 -strat perm -nodes 648 -simtime .100001 -pullrate 1 -flowfile flows_test.htsim
+../../src/clos/datacenter/htsim_ndp_fatTree_3to1_k12 -q 46 -cwnd 30 -strat perm -nodes 648 -simtime 10.00001 -pullrate 1 -flowfile flows_1percLoad_10sec_648hosts_3to1_prio_1.htsim >> FCT_3to1_pfab_cwnd30_1perc.txt
